c2378b321215d643d31c05e540c997d0647b04c9
Rithika L <rithika.l@tvsnext.com>
2023-08-30 15:48:57 +0530

    [Rithika] Add : Implemented

---
+++ ProductReviewManagement/Operation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Operation
+    {
+        public void Display(List<Product> list)
+        {
+            foreach (var data in list)
+            {
+                Console.WriteLine(data.ProductID + " " + data.UserID + " " + data.Rating + " "
+                    + data.Review + " " + data.isLike + " ");
+            }
+        }
+    }
+}
